3881323e52f00c9bb32edf7c54d0fee7ec223b41
luguo15 <112709254+luguo15@users.noreply.github.com>
2024-02-06 14:22:38 -0500

    add skills

---
+++ app/components/Test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            <h1>Lu Guo</h1>
+            <p>Hi, I'm Lu Guo. I'm a software engineer and a web developer. I'm passionate about web development, and I love to learn new things.</p>
+
+            <br />
+            <h2>Skills</h2>
+            <h3>Frontend</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Redux</li>
+                <li>Remix</li>
+                <li>Next.js</li>
+                <li>Gatsby</li>
+            </ul>
+
+        </div>
+    );
+}
+
+
+export default Test;
