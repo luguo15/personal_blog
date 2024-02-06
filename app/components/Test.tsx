c64776c719a6d8a45bf47714e230eeb52d6d2c9c
luguo15 <112709254+luguo15@users.noreply.github.com>
2024-02-06 14:52:56 -0500

    add skills

--- app/components/Test.tsx
+++ app/components/Test.tsx
@@ -4,21 +4,14 @@ const Test = () => {
     return (
         <div>
             <h1>Lu Guo</h1>
-            <p>Hi, I'm Lu Guo. I'm a software engineer and a web developer. I'm passionate about web development, and I love to learn new things.</p>
+            <text>Hi, I'm Lu Guo. I'm a software engineer and a web developer. I'm passionate about web development, and I love to learn new things.</text>
 
             <br />
-            <h2>Skills</h2>
-            <h3>Frontend</h3>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Redux</li>
-                <li>Remix</li>
-                <li>Next.js</li>
-                <li>Gatsby</li>
-            </ul>
+            <h2>Skills | Publication</h2>
+            <text><b>Research Paper: </b>Kim, Y., Guo, L., Yu, B., & Li, Y. (2023). Can ChatGPT Understand Causal Language in Science Claims? (Top-tier NLP conference: ACL)</text><br /> 
+            <text><b>Programming and System: </b>Python (TensorFlow, Keras, Pandas, Numpy), R, SQL, Git, HTML, XML, Linux</text><br /> 
+            <text><b>Data Analysis: </b>Machine Learning (regression, decision trees, random forests, k-means clustering, and Transformer), Deep Learning, Natural Language Processing, Data Visualization</text><br /> 
+            <text><b>Tools: </b>Power BI, Tableau, Google Analytics, Salesforce, Excel, Word, PowerPoint, Access, Illustrator, Visio, AWS, JIRA</text><br /> 
 
         </div>
     );
